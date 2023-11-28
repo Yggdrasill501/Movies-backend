af6bb8254773a9736759ac6a959b475a26529f37
Filip Žitný <filipzitny@gmail.com>
2023-11-28 01:47:01 +0100

    Bug

--- src/main/java/com/yggrasillwebendterm/webendterm/MovieController.java
+++ src/main/java/com/yggrasillwebendterm/webendterm/MovieController.java
@@ -1,13 +1,16 @@
 package com.yggrasillwebendterm.webendterm;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping ("/api/v1/movies")
@@ -17,5 +20,9 @@ public class MovieController {
     @GetMapping
     private ResponseEntity<List<Movie>> getAllMovies(){
         return new ResponseEntity<>(movieService.allMovies(), HttpStatus.OK);
+    }
+    @GetMapping("/{id}")
+    public Optional<Movie> getSingleMovie(@PathVariable ObjectId id){
+        return new ResponseEntity<Optional<Movie>>(MovieService.signleMovie(id), HttpStatus.OK);
     }
 }
--- src/main/java/com/yggrasillwebendterm/webendterm/MovieService.java
+++ src/main/java/com/yggrasillwebendterm/webendterm/MovieService.java
@@ -1,9 +1,11 @@
 package com.yggrasillwebendterm.webendterm;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
@@ -12,5 +14,8 @@ public class MovieService {
     private MovieRepostory movieRepostory;
     public List<Movie> allMovies(){
         return movieRepostory.findAll();
+    }
+    public Optional<Movie> singleMovie(ObjectId id){
+        return movieRepostory.findById(id);
     }
 }
